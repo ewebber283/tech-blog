f71d174dc959837bc4ee274b57a2981ea829cc97
Eric Webber <ewebber283@gmail.com>
2021-06-26 18:14:16 -0500

    added login, logout, and signup ability

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -52,12 +52,16 @@ router.post('/', (req, res) => {
       username: req.body.username,
       password: req.body.password
     })
-      .then(dbUserData => res.json(dbUserData))
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
+    .then(dbUserData => {
+      req.session.save(() => {
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
+    
+        res.json(dbUserData);
       });
-  });
+    })
+})
 
 router.post('/login', (req, res) => {
     User.findOne({
@@ -75,9 +79,24 @@ router.post('/login', (req, res) => {
         res.status(400).json({ message: 'Incorrect password!' });
         return;
       }
-  
+      req.session.save(() => {
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
       res.json({ user: dbUserData, message: 'You are now logged in!' });
     });
   });
+});
+
+router.post('/logout', (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  }
+  else {
+    res.status(404).end();
+  }
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
 const session = require('express-session')
+const routes = require('./controllers');
 const helpers = require('./utils/helpers');
 
 const app = express();
@@ -11,7 +12,7 @@ const sequelize = require('./config/connection');
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const sess = {
-  secret: 'Super secret secret',
+  secret: 'Secret Session',
   cookie: {},
   resave: false,
   saveUninitialized: true,
@@ -30,7 +31,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session(sess));
-app.use(require('./controllers/'));
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
