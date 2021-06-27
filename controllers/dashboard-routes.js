70014df870edaf75d436150fc0c16621802c931f
Eric Webber <ewebber283@gmail.com>
2021-06-26 20:44:06 -0500

    created dashboard and routes

---
+++ controllers/dashboard-routes.js
@@ -0,0 +1,41 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
+
+router.get('/', (req, res) => {
+    Post.findAll({
+      where: {
+        user_id: req.session.user_id
+      },
+      attributes: [
+        'id',
+        'post_url',
+        'body',
+        'title',
+        'created_at',
+      ],
+      include: [
+        {
+          model: Comment,
+          attributes: ['id', 'body', 'post_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    })
+      .then(dbPostData => {
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+        res.render('dashboard', { posts, loggedIn: true });
+      })
+      .catch(err => {
+        res.status(500).json(err);
+      });
+  });
+
+module.exports = router;
--- utils/auth.js
+++
@@ -1,9 +0,0 @@
-const withAuth = (req, res, next) => {
-    if(!req.session.use_id) {
-        res.redirect('/login');
-    } else {
-        next();
-    }
-};
-
-module.exports = withAuth;
